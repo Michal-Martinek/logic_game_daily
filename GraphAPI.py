ec85c49fd385ec28f029543a02a0eedd723b27ee
Michal Martínek <michal7952@seznam.cz>
2024-04-18 17:56:50 +0200

    Improved requests, added comment fetch

--- GraphAPI.py
+++ GraphAPI.py
@@ -1,27 +1,50 @@
 import requests
 import json
+import datetime
 
 # helpers ------------------------
 def getCredentials() -> dict:
 	with open('credentials.json') as f:
 		creds = json.load(f)
 	return creds
-def displayJson(data):
-	s = json.dumps(data, indent = 4)
+def displayJson(data, indent=4):
+	s = json.dumps(data, indent=indent)
 	print(s)
+
+class Comment: # TODO test
+	def __init__(self, comment: dict):
+		self.id: str = comment['id']
+		self.username: str = comment['from']['username']
+		self.userId: str = comment['from']['id']
+		self.text: str = comment['text']
+		self.likes: int = comment['like_count']
+		self.time = datetime.datetime.strptime(comment['timestamp'], r'%Y-%m-%dT%X%z')
+	def __str__(self) -> str:
+		return self.username  + ': "' + self.text + '"'
 
 CREDENTIALS = getCredentials()
 API_BASE_URL = 'https://graph.facebook.com/v19.0'
 ACCOUNT_SPECIFIC_URL = API_BASE_URL + '/' + CREDENTIALS['instagram_account_id']
 
+# requests --------------------------------
 def prepareParams(params):
 	params['access_token'] = CREDENTIALS['access_token']
+def handleRes(res: requests.Response) -> dict:
+	print(res.url.replace(CREDENTIALS['access_token'], '###'))
+	try:
+		data = json.loads(res.content)
+		displayJson(data)
+	except: print(res.content, end='\n\n')
+	assert res.ok, f'Request returned status code {res.status_code}'
+	return data
 def callApi(endpoint: str, method='GET', **params):
 	prepareParams(params)
-	url = API_BASE_URL + endpoint
+	url = API_BASE_URL + '/' * (endpoint[0] != '/') + endpoint
 	res = requests.request(method, url, params=params)
-	return json.loads(res.content)
+	return handleRes(res)
 
-
-r = callApi('/me')
-displayJson(r)
+def getComments(postId) -> list[Comment]:
+	res = callApi(postId + '/comments', postId, fields='id,text,like_count,from,timestamp')
+	comments = res['data']
+	comments = [Comment(c) for c in comments]
+	return comments
